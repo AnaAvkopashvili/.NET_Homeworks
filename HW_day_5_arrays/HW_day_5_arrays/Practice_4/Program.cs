16503f9eebb32db8497d2f1a6018b9ac7178fd6e
kiu_avkopashvili.ana <avkopashvili.ana@kiu.edu.ge>
2023-11-23 00:31:13 +0400

    finished hw_day_5_arrays

---
+++ HW_day_5_arrays/HW_day_5_arrays/Practice_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter array size: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] arr = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Enter number for index " + i + " : ");
+    int index = int.Parse(Console.ReadLine());
+    arr[i] = index;
+}
+
+int product= 1;
+foreach (int n in arr)
+{
+    product *= n;
+}
+Console.Write("Product of array elements is: " + product);
+
---
+++ HW_day_5_arrays/HW_day_5_arrays/Practice_7/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Write("Enter row size: ");
+int size1 = int.Parse(Console.ReadLine());
+Console.Write("Enter column size: ");
+int size2 = int.Parse(Console.ReadLine());
+
+int[,] arr1 = new int[size1, size2];
+int[,] arr2 = new int[size1, size2];
+
+Console.WriteLine("Fill first matrix");
+for (int i = 0; i < size1; i++)
+{ 
+    for (int j = 0; j < size2; j++)
+    {
+        {
+            Console.Write("Enter number for index " + i + "," + j + " : ");
+            int index = int.Parse(Console.ReadLine());
+            arr1[i, j] = index;
+        }
+    }
+ }
+
+Console.WriteLine("Fill second matrix");
+
+for (int i = 0; i <size1; i++)
+{
+    for (int j = 0; j < size2; j++)
+    {
+        {
+            Console.Write("Enter number for index " + i + "," + j + " : ");
+            int index = int.Parse(Console.ReadLine());
+            arr2[i, j] = index;
+        }
+    }
+}
+
+Console.WriteLine("Here is the sum of 2 matrices");
+for (int i = 0; i < size1; i++)
+{
+    for (int j = 0; j < size2;j++)
+    {
+        Console.Write(arr1[i, j] + arr2[i, j] + " "); 
+    }
+    Console.WriteLine("");
+}
+
+
+
