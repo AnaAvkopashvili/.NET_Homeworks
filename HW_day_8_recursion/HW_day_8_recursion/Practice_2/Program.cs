54ff82a62ffcaee96225ba30c3b9afabb56821a0
kiu_avkopashvili.ana <avkopashvili.ana@kiu.edu.ge>
2023-12-01 11:30:07 +0400

    finnished hw_day_8_recursion

---
+++ HW_day_8_recursion/HW_day_8_recursion/Practice_2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(13));
+            Console.WriteLine(sumTail(13, 0));
+        }
+        static int sum(int n)
+        {
+            if (n == 0) 
+                return 0;  
+            else
+                return n += sum(n - 1);
+            
+        }
+        static int sumTail(int n, int acc)
+        {
+            if (n == 0)
+                return acc;
+            else 
+                return sumTail(n - 1, acc + n);
+        }
+    }
+}
---
+++ HW_day_8_recursion/HW_day_8_recursion/Practice_4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(power(2, 10));
+            Console.WriteLine(powerTail(2, 10, 1));
+
+        }
+        static int power(int n, int degree)
+        {
+            if (degree == 0)
+                return 1;
+            else
+                return n * power(n, degree - 1);
+        }
+        static int powerTail(int n, int degree, int acc)
+        {
+            if (degree == 0)
+                return acc;
+            else
+                return powerTail(n, degree - 1, n * acc);
+        }
+    }
+}
