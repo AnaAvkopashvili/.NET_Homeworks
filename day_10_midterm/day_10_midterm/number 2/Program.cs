f987c552cb89ee1faf7044f0c03369547a83f063
kiu_avkopashvili.ana <avkopashvili.ana@kiu.edu.ge>
2023-12-02 12:49:22 +0400

    midterm

---
+++ day_10_midterm/day_10_midterm/number 2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace number_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 4, 1, 2, 3, 7 };
+            Console.WriteLine(dissapeared(array));
+        }
+        static int dissapeared(int[] array)
+        {
+            int sum = 0;
+            for (int j = 0; j <= array.Length + 1; j++)
+            {
+                sum += j;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum -= array[i];
+            }
+            return sum;
+        }
+    }
+}
---
+++ day_10_midterm/day_10_midterm/number 4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace number_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To turn off program type \"exit\" otherwise enter the directory path");
+            string mainDir = "C: \\Users\\Kiu - Student\\Desktop";
+            files(mainDir);
+        }
+        static void files(string input)
+        {
+            if (input == "exit")
+                return;
+
+            if (Directory.Exists(input))
+                Console.WriteLine($"The directory {input} does not exist!");
+            else
+            {
+                Console.WriteLine(Directory.GetFiles(input));
+                //files(Directory.GetDirectories(input));
+            }
+        }
+    }
+}
+
