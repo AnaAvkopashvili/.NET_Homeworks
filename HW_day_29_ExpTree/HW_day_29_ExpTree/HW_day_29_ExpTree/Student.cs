f38133f67e60b73e6709aa2c60eeeab6c0021ba2
kiu_avkopashvili.ana <avkopashvili.ana@kiu.edu.ge>
2024-01-26 14:33:06 +0400

    finished HW_Day_29_ExpTree

---
+++ HW_day_29_ExpTree/HW_day_29_ExpTree/HW_day_29_ExpTree/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_day_29_ExpTree
+{
+    public class Student
+    {
+        public int Age { get; set; }
+        public string Name {  get; set; }
+        public char Group {  get; set; }
+        public Student(string name, int age, char group)
+        {
+            Age = age;
+            Name = name;
+            Group = group;
+        }
+
+    }
+}
