152479a291bfe6a372b0dbc938a0442efd368f15
kiu_avkopashvili.ana <avkopashvili.ana@kiu.edu.ge>
2024-02-10 15:32:08 +0400

    midterm_3

---
+++ Midterm_3/Midterm_3/Midterm_3/Extension.cs
@@ -0,0 +1,62 @@
+﻿namespace Midterm_3
+{
+    internal static class Extension<T>
+    {
+        private static T[] arr;
+        private static int size;
+        public static T First(Predicate<T> predicate)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (predicate(arr[i]))
+                {
+                    return arr[i];
+                }
+            }
+            throw new InvalidOperationException("The element you are trying to find could not be found");
+        }
+
+        public static T FirstOrDefault(Predicate<T> predicate)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (predicate(arr[i]))
+                {
+                    return arr[i];
+                }
+            }
+            return default(T);
+        }
+
+        public static IEnumerable<T> Where(Func<T, bool> predicate)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (predicate(arr[i]))
+                {
+                    yield return arr[i];
+                }
+            }
+            throw new InvalidOperationException("The elements you are trying to find could not be found");
+        }
+        public static IEnumerable<T> Distinct(Func<T, bool> predicate)
+        {
+            List<T> lst = new List<T>();
+            for (int i = 0; i < size; i++)
+            {
+                if (predicate(arr[i]))
+                {
+                    if (!lst.Contains(arr[i]))
+                    {
+                        lst.Add(arr[i]);
+                    }
+                 throw new InvalidOperationException("The elements you are trying to find could not be found");
+                }
+            }
+            foreach(var item in lst)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ Midterm_3/Midterm_3/MyList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MyList
+{
+    internal class Program 
+    { 
+        static void Main(string[] args)
+        {
+            List<int> mylist = new List<int>();
+            mylist.Add(2);
+        }
+    }
+}
